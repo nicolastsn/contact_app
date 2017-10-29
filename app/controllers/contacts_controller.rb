5e4e98f9e0a83eefb962a83fdcffed42fc9c7446
Nicolas <nicolas.tsan@gmail.com>
2017-10-29 13:41:41 +0100

    order by alphabetic lastname

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,7 +4,11 @@ class ContactsController < ApplicationController
   # GET /contacts
   # GET /contacts.json
   def index
-    @contacts = Contact.all
+    if params[:tag]
+      @contacts = Contact.tagged_with(params[:tag])
+    else
+      @contacts = Contact.all.order('last_name ASC')
+    end
   end
 
   # GET /contacts/1
