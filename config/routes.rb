35682c34f9a8f8955d717742f4eccd20cea444f3
Nicolas <nicolas.tsan@gmail.com>
2017-10-29 12:54:56 +0100

    tag system ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
 
 	root 'contacts#index'
   resources :contacts
+
+  get 'tags/:tag', to: 'contacts#index', as: :tag
+
+
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
